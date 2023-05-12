2cb8bcd2ed4607bdb3f222d158fefff93af17cab
Darlan Schwartz <darlanswtz@gmail.com>
2023-05-12 06:24:01 -0300

    Mudanças css nos efeitos de hover e disabled de botões

--- src/Chute.js
+++ src/Chute.js
@@ -18,7 +18,7 @@ export default function Chute(props)
         <div className="guess-word-container">
             <p>Já sei a palavra!</p>
             {!hasEndedGame() ? <input data-test="guess-input" onChange={(e)=> setGuessWord(e.target.value)} type="text"></input> : <input  disabled data-test="guess-input" onChange={(e)=> setGuessWord(e.target.value)} type="text"></input>}
-            <button data-test="guess-button" onClick={() =>props.onChange(guessWord)} >Chutar</button>
+            {!hasEndedGame() ? <button data-test="guess-button" onClick={() =>props.onChange(guessWord)} >Chutar</button> : <button disabled data-test="guess-button" onClick={() =>props.onChange(guessWord)} >Chutar</button>}
         </div>
     );
 }
